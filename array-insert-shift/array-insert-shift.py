a599a35640e985a445b752c536a91b34d64c859d
hind.hbashneh <75991604+hind-hb@users.noreply.github.com>
2022-03-02 18:39:53 +0300

    Update array-insert-shift.py

--- array-insert-shift/array-insert-shift.py
+++ array-insert-shift/array-insert-shift.py
@@ -1,10 +1,9 @@
-def insertShiftArray(arr,val):
-    Arrnew=[]
-    arr1=arr[:int(len(arr)/2)]
-    arr2=arr[int(len(arr)/2):]
-    arr1.append(val)
-    Arrnew=arr1+arr2  
-    return Arrnew  
-if __name__ == "__main__":
-    print(insertShiftArray([2, 4, 6, 8], 5))
-    print(insertShiftArray([42,8,15,23,42], 16))
+import math
+def insertShiftArray(arr, val):
+
+    e = math.ceil(len(arr)/2)
+
+    print(arr[:e] + [val] + arr[e:])
+
+    return(arr[:e] + [val] + arr[e:])
+insertShiftArray([1, 2, 3, 4, 5, 6], 15)

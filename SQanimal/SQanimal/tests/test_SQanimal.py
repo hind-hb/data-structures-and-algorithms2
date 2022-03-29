57a74a606fcf365707f3848627cbf28c061b17a6
hind-hb <hind.hbashenh@gmail.com>
2022-03-29 17:30:19 +0300

    add SQanimal

---
+++ SQanimal/SQanimal/tests/test_SQanimal.py
@@ -0,0 +1,29 @@
+from sqanimal.Stack_Queue_animal import AnimalShelter , Node
+
+
+
+def test_enqueue():
+  x = AnimalShelter()
+  x.enqueue("dog")
+  actual = x.start.value
+  expected = "dog"
+  assert actual == expected
+
+
+
+def test_dequeue():
+  x = AnimalShelter()
+  x.enqueue("cat")
+  actual = x.dequeue("cat")
+  expected = "cat"
+  assert actual == expected
+
+
+
+def test_pref_dog_no_cat_no():
+  x = AnimalShelter()
+  x.enqueue("cat")
+  x.enqueue("dog")
+  actual = x.dequeue("zebra")
+  expected = "null"
+  assert actual == expected
